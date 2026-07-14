62e16b18cd7b11ed0b44d1b9a3781b4c7af02381
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 11:33:03 +0530

    Add account header

---
+++ MiniProjects/BankingManagementSystem/include/account.h
@@ -0,0 +1,20 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#define MAX_ACCOUNTS 100
+
+typedef struct
+{
+    int accountNumber;
+    char accountHolder[50];
+    float balance;
+} Account;
+
+extern Account accounts[MAX_ACCOUNTS];
+extern int totalAccounts;
+
+void createAccount();
+void displayAllAccounts();
+void searchAccount();
+
+#endif
