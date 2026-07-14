5acc4b8f03831a646bb788d57e2995e5c4d2709c
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 11:35:57 +0530

    Create account module

---
+++ MiniProjects/BankingManagementSystem/src/account.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "../include/account.h"
+
+Account accounts[MAX_ACCOUNTS];
+int totalAccounts = 0;
+
+void createAccount()
+{
+    printf("Create Account Function\n");
+}
+
+void displayAllAccounts()
+{
+    printf("Display All Accounts Function\n");
+}
+
+void searchAccount()
+{
+    printf("Search Account Function\n");
+}
