fd7559080b21411fd54aaf9bcfe127ea86b47bed
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 11:43:28 +0530

    Implement deposit, withdrawal and balance operations

--- MiniProjects/BankingManagementSystem/src/transaction.c
+++ MiniProjects/BankingManagementSystem/src/transaction.c
@@ -1,17 +1,81 @@
 #include <stdio.h>
+#include "../include/account.h"
 #include "../include/transaction.h"
 
 void depositMoney()
 {
-    printf("Deposit Function\n");
+    int accountNumber,i;
+    float amount;
+
+    printf("\nEnter Account Number : ");
+    scanf("%d",&accountNumber);
+
+    for(i=0;i<totalAccounts;i++)
+    {
+        if(accounts[i].accountNumber==accountNumber)
+        {
+            printf("Enter Amount : ");
+            scanf("%f",&amount);
+
+            accounts[i].balance+=amount;
+
+            printf("\nAmount Deposited Successfully.\n");
+            printf("Updated Balance : %.2f\n",accounts[i].balance);
+            return;
+        }
+    }
+
+    printf("\nAccount Not Found.\n");
 }
 
 void withdrawMoney()
 {
-    printf("Withdraw Function\n");
+    int accountNumber,i;
+    float amount;
+
+    printf("\nEnter Account Number : ");
+    scanf("%d",&accountNumber);
+
+    for(i=0;i<totalAccounts;i++)
+    {
+        if(accounts[i].accountNumber==accountNumber)
+        {
+            printf("Enter Amount : ");
+            scanf("%f",&amount);
+
+            if(amount>accounts[i].balance)
+            {
+                printf("\nInsufficient Balance.\n");
+                return;
+            }
+
+            accounts[i].balance-=amount;
+
+            printf("\nWithdrawal Successful.\n");
+            printf("Remaining Balance : %.2f\n",accounts[i].balance);
+            return;
+        }
+    }
+
+    printf("\nAccount Not Found.\n");
 }
 
 void checkBalance()
 {
-    printf("Check Balance Function\n");
+    int accountNumber,i;
+
+    printf("\nEnter Account Number : ");
+    scanf("%d",&accountNumber);
+
+    for(i=0;i<totalAccounts;i++)
+    {
+        if(accounts[i].accountNumber==accountNumber)
+        {
+            printf("\nAccount Holder : %s\n",accounts[i].accountHolder);
+            printf("Current Balance : %.2f\n",accounts[i].balance);
+            return;
+        }
+    }
+
+    printf("\nAccount Not Found.\n");
 }
