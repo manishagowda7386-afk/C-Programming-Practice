b7f78e459934f9b471674bf905bfc16372131d43
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 12:26:13 +0530

    Improve user menu interface

--- MiniProjects/BankingManagementSystem/src/menu.c
+++ MiniProjects/BankingManagementSystem/src/menu.c
@@ -3,14 +3,16 @@
 
 void displayMenu()
 {
-    printf("\n===============================\n");
-    printf(" BANKING MANAGEMENT SYSTEM\n");
-    printf("===============================\n");
+    printf("\n");
+    printf("=============================================\n");
+    printf("      BANKING MANAGEMENT SYSTEM\n");
+    printf("=============================================\n");
     printf("1. Create Account\n");
     printf("2. Deposit Money\n");
     printf("3. Withdraw Money\n");
     printf("4. Check Balance\n");
     printf("5. Search Account\n");
-    printf("6. View All Accounts\n");
+    printf("6. Display All Accounts\n");
     printf("7. Exit\n");
+    printf("=============================================\n");
 }
