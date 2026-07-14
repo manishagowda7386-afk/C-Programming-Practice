b5da296454578a76b258da787324b45713c13365
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 11:39:22 +0530

    Implement account management

--- MiniProjects/BankingManagementSystem/src/account.c
+++ MiniProjects/BankingManagementSystem/src/account.c
@@ -1,20 +1,70 @@
 #include <stdio.h>
+#include <string.h>
 #include "../include/account.h"
 
 Account accounts[MAX_ACCOUNTS];
-int totalAccounts = 0;
+int totalAccounts=0;
 
 void createAccount()
 {
-    printf("Create Account Function\n");
+    if(totalAccounts>=MAX_ACCOUNTS)
+    {
+        printf("\nDatabase Full!\n");
+        return;
+    }
+
+    printf("\nEnter Account Number : ");
+    scanf("%d",&accounts[totalAccounts].accountNumber);
+
+    printf("Enter Account Holder : ");
+    scanf(" %[^\n]",accounts[totalAccounts].accountHolder);
+
+    printf("Enter Initial Balance : ");
+    scanf("%f",&accounts[totalAccounts].balance);
+
+    totalAccounts++;
+
+    printf("\nAccount Created Successfully.\n");
 }
 
 void displayAllAccounts()
 {
-    printf("Display All Accounts Function\n");
+    int i;
+
+    if(totalAccounts==0)
+    {
+        printf("\nNo Accounts Found.\n");
+        return;
+    }
+
+    printf("\n=========== ACCOUNT LIST ===========\n");
+
+    for(i=0;i<totalAccounts;i++)
+    {
+        printf("\nAccount %d\n",i+1);
+        printf("Number : %d\n",accounts[i].accountNumber);
+        printf("Name   : %s\n",accounts[i].accountHolder);
+        printf("Balance: %.2f\n",accounts[i].balance);
+    }
 }
 
 void searchAccount()
 {
-    printf("Search Account Function\n");
+    int number,i;
+
+    printf("\nEnter Account Number : ");
+    scanf("%d",&number);
+
+    for(i=0;i<totalAccounts;i++)
+    {
+        if(accounts[i].accountNumber==number)
+        {
+            printf("\nAccount Found\n");
+            printf("Name : %s\n",accounts[i].accountHolder);
+            printf("Balance : %.2f\n",accounts[i].balance);
+            return;
+        }
+    }
+
+    printf("\nAccount Not Found.\n");
 }
