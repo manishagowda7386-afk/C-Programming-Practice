3e68f5ecc06d43d87e21f1cd315b12061ce606be
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 17:03:57 +0530

    Add bank dashboard option to menu

--- MiniProjects/BankingManagementSystem/src/menu.c
+++ MiniProjects/BankingManagementSystem/src/menu.c
@@ -6,14 +6,16 @@ void displayMenu()
     printf("\n=================================================\n");
     printf("          BANKING MANAGEMENT SYSTEM\n");
     printf("=================================================\n");
-    printf("1. Create Account\n");
-    printf("2. Deposit Money\n");
-    printf("3. Withdraw Money\n");
-    printf("4. Check Balance\n");
-    printf("5. Search Account\n");
-    printf("6. Display All Accounts\n");
-    printf("7. Update Account\n");
-    printf("8. Delete Account\n");
-    printf("9. Exit\n");
+    printf("1.  Create Account\n");
+    printf("2.  Deposit Money\n");
+    printf("3.  Withdraw Money\n");
+    printf("4.  Check Balance\n");
+    printf("5.  Search Account\n");
+    printf("6.  Display All Accounts\n");
+    printf("7.  Update Account\n");
+    printf("8.  Delete Account\n");
+    printf("9.  View Transaction History\n");
+    printf("10. Bank Dashboard\n");
+    printf("11. Exit\n");
     printf("=================================================\n");
 }
