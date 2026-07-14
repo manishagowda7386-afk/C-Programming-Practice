eda8b86953f042c3cfe5b65cc13f810627d82ecf
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 16:58:16 +0530

    Update transaction header for transaction history support

--- MiniProjects/BankingManagementSystem/include/transaction.h
+++ MiniProjects/BankingManagementSystem/include/transaction.h
@@ -1,8 +1,15 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
+/* PIN Verification */
+int verifyPin(int index);
+
+/* Transaction Operations */
 void depositMoney();
 void withdrawMoney();
 void checkBalance();
+
+/* Transaction History */
+void viewTransactionHistory();
 
 #endif
