7636c527291c70e5bbecdc97c13040217ca520de
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 12:32:34 +0530

    Expand application menu

--- MiniProjects/BankingManagementSystem/src/menu.c
+++ MiniProjects/BankingManagementSystem/src/menu.c
@@ -3,16 +3,17 @@
 
 void displayMenu()
 {
-    printf("\n");
-    printf("=============================================\n");
-    printf("      BANKING MANAGEMENT SYSTEM\n");
-    printf("=============================================\n");
+    printf("\n=================================================\n");
+    printf("          BANKING MANAGEMENT SYSTEM\n");
+    printf("=================================================\n");
     printf("1. Create Account\n");
     printf("2. Deposit Money\n");
     printf("3. Withdraw Money\n");
     printf("4. Check Balance\n");
     printf("5. Search Account\n");
     printf("6. Display All Accounts\n");
-    printf("7. Exit\n");
-    printf("=============================================\n");
+    printf("7. Update Account\n");
+    printf("8. Delete Account\n");
+    printf("9. Exit\n");
+    printf("=================================================\n");
 }
