cb4b435d845dcf2856823236f01747411f89449f
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 11:37:13 +0530

    Create transaction module

---
+++ MiniProjects/BankingManagementSystem/src/transaction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../include/transaction.h"
+
+void depositMoney()
+{
+    printf("Deposit Function\n");
+}
+
+void withdrawMoney()
+{
+    printf("Withdraw Function\n");
+}
+
+void checkBalance()
+{
+    printf("Check Balance Function\n");
+}
