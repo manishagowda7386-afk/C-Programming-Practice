f3b3a0c1650fdbfaf5cc9713bb88fb323f909cc8
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 11:36:40 +0530

    Create menu module

---
+++ MiniProjects/BankingManagementSystem/src/menu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "../include/menu.h"
+
+void displayMenu()
+{
+    printf("\n===============================\n");
+    printf(" BANKING MANAGEMENT SYSTEM\n");
+    printf("===============================\n");
+    printf("1. Create Account\n");
+    printf("2. Deposit Money\n");
+    printf("3. Withdraw Money\n");
+    printf("4. Check Balance\n");
+    printf("5. Search Account\n");
+    printf("6. View All Accounts\n");
+    printf("7. Exit\n");
+}
