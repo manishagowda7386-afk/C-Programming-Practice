1d684d79836e76ca351175fd003bd0276dc23f2d
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-14 18:44:33 +0530

    Create validation module header

---
+++ MiniProjects/BankingManagementSystem/include/validation.h
@@ -0,0 +1,9 @@
+#ifndef VALIDATION_H
+#define VALIDATION_H
+
+int isValidPin(int pin);
+int isValidAmount(float amount);
+int isValidAccountNumber(int accountNumber);
+int isValidName(char name[]);
+
+#endif
